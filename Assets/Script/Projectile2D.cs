5ecef88b5a7803c1a7ef43ddedfd555918a26a65
Zuibernard <zuibernard27@gmail.com>
2026-04-28 14:25:27 +0700

    Projectile2DCode

--- Assets/Script/Projectile2D.cs
+++ Assets/Script/Projectile2D.cs
@@ -1,16 +1,36 @@
 using UnityEngine;
+using UnityEngine.InputSystem; 
 
-public class Projectile2D : MonoBehaviour
+public class Shooter : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private Transform shootPoint;     
+    [SerializeField] private GameObject target;         
+    [SerializeField] private GameObject bulletPrefab;
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector2 screenPos = Mouse.current.position.ReadValue();
+
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+           
+            Ray ray = Camera.main.ScreenPointToRay(screenPos);
+            Debug.DrawRay(ray.origin, ray.direction * 5f, Color.red, 5f);
+
+          
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
+
+            
+            if (hit.collider != null)
+            {
+                target.transform.position = new Vector2(hit.point.x, hit.point.y);
+                Debug.Log($"Hit {hit.collider.gameObject.name}");
+            }
+        }
     }
 }
